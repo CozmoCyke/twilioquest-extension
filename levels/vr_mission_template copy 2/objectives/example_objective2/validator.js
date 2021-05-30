e6950fd8507a7e533e6ce78d62b43a8a4c9fba9c
Muthu Aiswaryaa Swaminathan <withlove.aish@gmail.com>
2021-05-30 19:00:40 +0530

    bug fix

--- levels/vr_mission_template copy 2/objectives/example_objective2/validator.js
+++ levels/vr_mission_template copy 2/objectives/example_objective2/validator.js
@@ -1,4 +1,3 @@
 module.exports = async helper => {
-  if(helper.validationFields == 'print(model.predict([10.0]))')
-  helper.success("Believe it or not, you covered most of the concepts in ML that you'll use in far more complex scenarios. CONGRATULATIONS!!")
+  helper.success("Believe it or not, you covered most of the concepts in ML that you'll use in far more complex scenarios. CONGRAULATIONS!!")
 };
